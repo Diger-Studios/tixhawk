be905d8970b4d376f8e32e466e1f256fa796785e
Rudy J. Ellis <rudy@Diger-M4.local>
2025-08-09 10:17:04 -0700

    build: replace Tailwind CDN with CLI; add build step and styles.css; set mp4 caching headers

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./thanks.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
